d74002d6e25759bb666ce3a51a05bb6a830c28f2
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-06-21 15:57:49 -0700

    forgot to make a dev branch, but will after this. set up the db and created endpoints for get routes

--- data/create-tables.js
+++ data/create-tables.js
@@ -17,10 +17,13 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE owls (
                     id SERIAL PRIMARY KEY NOT NULL,
                     name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
+                    note VARCHAR(512) NOT NULL,
+                    endangered BOOLEAN NOT NULL,
+                    habitat VARCHAR(512) NOT NULL,
+                    price DECIMAL NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
---
+++ data/owls.js
@@ -0,0 +1,48 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Barn Owl',
+    // image: '/barn-owl-.jpeg',
+    note: 'Besides excellent vision, Barn Owls are extraordinarily sensitive to sound; their ability to locate prey by sound alone is the best of any animal ever tested.',
+    // threats: 'Habitat loss, vehicle collisions',
+    endangered: false,
+    habitat: 'Grasslands, deserts, and agricultural fields',
+    price: 600.00,
+  }, {
+    id: 2,
+    name: 'Barred Owl',
+    // image: '/barred-owl.jpeg',
+    note: 'Barred Owls expanded their range dramatically in the 20th century to include western forests, where they now compete with Spotted Owls.',
+    // threats: 'Deforestation',
+    endangered: false,
+    habitat: 'Mixed, mature forests',
+    price: 400.00,
+  }, {
+    id: 3,
+    name: 'Ferruginous Pygmy',
+    // image: '/ferruginous-pygmy.jpeg',
+    note: 'The Ferruginous Pygmy-Owl is one of the most widespread birds of the Neotropical lowlands; its range includes the southern tip of Texas and the south-central edge of Arizona.',
+    // threats: 'Habitat loss, land conversion, and urbanization',
+    endangered: true,
+    habitat: 'Tropical dry forests',
+    price: 1300.00,
+  }, {
+    id: 4,
+    name: 'Great Horned Owl',
+    // image: '/ferruginous-pygmy.jpeg',
+    note: 'Great Horned Owls hunt Ospreys, Peregrine Falcons, and other types of owls, along with many other animals.',
+    // threats: 'Habitat loss, land conversion, and urbanization',
+    endangered: true,
+    habitat: 'Wide variety of habitat, including forests and fields',
+    price: 1500.00,
+  }, {
+    id: 5,
+    name: 'Northern Hawk Owl',
+    // image: '/ferruginous-pygmy.jpeg',
+    note: 'Northern Hawk Owls can spot prey at distances up to half a mile away.',
+    // threats: 'Habitat loss, land conversion, and urbanization',
+    endangered: true,
+    habitat: 'Boreal forests',
+    price: 2000.00,
+  }
+];
