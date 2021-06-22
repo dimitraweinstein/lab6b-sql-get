1f40218f89a426a40c92e695100660dd3adfe850
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-06-21 23:40:47 -0700

    created joins branch for lab 8 work

--- __tests__/post-put-delete.js
+++ __tests__/post-put-delete.js
@@ -64,36 +64,35 @@ describe('app routes', () => {
     });
   });
 
-//   test('/DELETE Creates single owl', async() => {
+  test('/PUT Updates single owl', async() => {
     
-//     const data = await fakeRequest(app)
-//       .post('/owls')
-//       .send({
-//         name: 'new owl',
-//         note: 'some note',
-//         endangered: true,
-//         habitat: 'forest',
-//         price: 1000
-//       })
-//       .expect('Content-Type', /json/)
-//       .expect(200);
+    const data = await fakeRequest(app)
+      .put('/owls/6')
+      .send({
+        name: 'updated new owl',
+        note: 'some note, like others',
+        endangered: true,
+        habitat: 'forest, mountains',
+        price: 1200
+      })
+      .expect('Content-Type', /json/)
+      .expect(200);
   
-//     const dataOwls = await fakeRequest(app)
-//       .get('/owls')
-//       .expect('Content-Type', /json/)
-//       .expect(200);
+    const dataOwls = await fakeRequest(app)
+      .get('/owls')
+      .expect('Content-Type', /json/)
+      .expect(200);
   
-//     const newOwl = {
-//       'id': 6, 
-//       'name': 'new owl', 
-//       'note': 'some note', 
-//       'endangered': true, 
-//       'habitat': 'forest',
-//       'price': '1000',
-//       'owner_id': 1, 
-//     };
-
-//     expect(dataOwls.body).not.toContainEqual(newOwl);
-//   });
-// });
-});  
+    const newOwl = {
+      'id': 6, 
+      'name': 'new owl', 
+      'note': 'some note', 
+      'endangered': true, 
+      'habitat': 'forest',
+      'price': '1000',
+      'owner_id': 1, 
+    };
+    expect(dataOwls.body).toEqual(newOwl);
+    expect(dataOwls.body).toContainEqual(newOwl);
+  });
+});
